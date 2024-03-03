53a146d024a6372db02403806733ac0954ccf85a
mohdshahzil <theshahzil@gmail.com>
2024-03-04 00:37:31 +0530

    text-style

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 36, 3, 94),
-        body: Center(
-          child: Text("Hello World!"),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
