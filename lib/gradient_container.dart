923022194984c5bc5e52f35d1c535a46dfe0f0f4
mohdshahzil <theshahzil@gmail.com>
2024-03-04 02:05:27 +0530

    button-addition

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,6 +8,9 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
   final Color color1;
   final Color color2;
+
+  void rollDice() {}
+
   @override
   Widget build(context) {
     return Container(
@@ -19,9 +22,17 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 200,
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            )
+          ],
         ),
       ),
     );
